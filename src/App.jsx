74f07255659837693cd295fe08bdab881bc97a7b
jvdat15@gmail.com <jvdat15@gmail.com>
2021-04-26 23:31:51 +0700

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import Login from "./components/Login/Login";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <a className="btn" href="">Login with Spotify</a>
+        </div>
+    )
+}
+
+export default Login
